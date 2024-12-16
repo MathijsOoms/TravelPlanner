62633924367a8f67ae299590ead0d92a032a463f
Lucasvandenheede <r0950888@student.thomasmore.be>
2024-12-16 22:31:11 +0100

    Start Booking service

---
+++ booking-servcie/src/main/java/com/travelplanner/bookingservcie/dto/BookingResponse.java
@@ -0,0 +1,24 @@
+package com.travelplanner.bookingservcie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingResponse {
+    private Long id;
+    private String customerName;
+    private String customerEmail;
+    private String destination;
+    private LocalDate startTime;
+    private LocalDate endTime;
+    private int numberOfPersons;
+    private double price;
+    private String status;
+}
